8861c30e331dc09e3e6ddf53fb7b9722df28cbbf
aploftus <aploftus@users.noreply.github.com>
2017-12-16 13:17:18 -0800

    (test) Add events routes responses tests

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const port = 3000;
+module.exports.port = port;
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening at http://localhost:3000!'));
+app.listen(port, () => console.log(`App listening at http://localhost:${port}!`));
