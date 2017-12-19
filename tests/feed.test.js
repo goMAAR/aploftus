56f4ed2d9b54c281010178018d6977f9a5f6fac2
aploftus <aploftus@users.noreply.github.com>
2017-12-19 14:44:55 -0800

    (test) Update tests for async callback style

--- tests/feed.test.js
+++ tests/feed.test.js
@@ -1,69 +1,67 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const supertest = require('supertest');
-const helpers = require('../server/helpers.js');
+const utils = require('../server/utils.js');
 const server = require('../server/index.js');
 
 const request = supertest.agent(server);
 
 describe('Client Service Feed Handler', () => {
-  describe('serveFeed', () => {
+  describe('serveLocalFeed', () => {
     it('should serve tweets from the cache', (done) => {
       const feed = [1, 2, 3, 4, 5];
-      const result = helpers.serveFeed(feed);
-
-      expect(result).to.be.an('array');
-      expect(result[0]).to.be.an('object');
-      expect(result.length).to.equal(5);
-      done();
+      utils.serveLocalFeed(feed, (result) => {
+        expect(result).to.be.an('array');
+        expect(result[0]).to.be.an('object');
+        expect(result.length).to.equal(5);
+        done();
+      });
     });
+  });
 
-    it('should fetch a tweet from Social Network Processing if the tweet is not in the cache', () => {
-      sinon.spy(helpers, 'fetchTweet');
+  describe('userAccessedInLast10Min', () => {
+    it('should return a boolean', (done) => {
+      const userId = 1;
 
-      const feed = [1, -1, 3, 4, 5];
-      const result = helpers.serveFeed(feed);
-
-      expect(helpers.fetchTweet).to.have.been.called;
-      done();
-
+      utils.userAccessedInLast10Min(userId, (boolean) => {
+        expect(boolean).to.be.a('boolean');
+        done();
+      });
     });
   });
 
   describe('refreshTweets', () => {
     it('should parse tweet objects into cache', (done) => {
-      const result = helpers.refreshTweets(
-        [
-          {
-            text: 'Today made me realize footlong sandwiches from Subway are amazing',
-            truncated: false,
-            created_at: 'Wed Apr 26 12:52:17 -0700 2017',
-            reply_count: 67,
-            favorite_count: 93,
-            favorited: false,
-            retweet_count: 107,
-            retweeted: true,
-            in_reply_to_user_id: 'null',
-            in_reply_to_screen_name: 'null',
-            in_reply_to_status_id: 'null',
-            possibly_sensitive: false,
-            source: 'Safari for iOS',
-            'user:id': 'testUser',
-            id: 0
-          }
-        ]
-      );
-
-      expect(result).to.equal(1);
-      done();
+      const newTweets = [
+        {
+          text: 'Today made me realize footlong sandwiches from Subway are amazing',
+          truncated: false,
+          created_at: 'Wed Apr 26 12:52:17 -0700 2017',
+          reply_count: 67,
+          favorite_count: 93,
+          favorited: false,
+          retweet_count: 107,
+          retweeted: true,
+          in_reply_to_user_id: 'null',
+          in_reply_to_screen_name: 'null',
+          in_reply_to_status_id: 'null',
+          possibly_sensitive: false,
+          source: 'Safari for iOS',
+          'user:id': -1,
+          id: -1
+        }
+      ];
+      utils.refreshTweets(newTweets, (result) => {
+        expect(result).to.equal(1);
+        done();
+      });
     });
-
   });
 
   describe('GET requests to /feed', () => {
     it('should respond with a user feed', (done) => {
       const body = {
-        user_id: 'testUser',
+        user_id: -1,
         count: 5
       };
 
