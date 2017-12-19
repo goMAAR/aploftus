5636b23840f26acc6aed4bb2a73a8d783952783e
aploftus <aploftus@users.noreply.github.com>
2017-12-18 16:19:46 -0800

    (test) Test helper function to send events to other services

--- tests/events.test.js
+++ tests/events.test.js
@@ -1,5 +1,8 @@
 const { expect } = require('chai');
+const moment = require('moment');
+const sinon = require('sinon');
 const supertest = require('supertest');
+const helpers = require('../server/helpers.js');
 const server = require('../server/index.js');
 
 const request = supertest.agent(server);
@@ -17,6 +20,23 @@ describe('Client Service Events', () => {
         .send(body)
         .expect(201, done);
     });
+
+    it('should send favorite event to User Engagement Analysis', () => {
+      sinon.spy(helpers, 'sendEvent');
+
+      const body = {
+        tweet_id: 'testTweet',
+        favoriter_id: 'testUser',
+      };
+
+      request
+        .post('/favorite/create')
+        .send(body)
+        .then(res => {
+          expect(helpers.sendEvent.to.have.been.calledWith('engagement', '/favorite/create', body));
+          done();
+        });
+    });
   });
 
   describe('POST requests to /favorite/destroy', () => {
@@ -30,6 +50,23 @@ describe('Client Service Events', () => {
         .post('/favorite/destroy')
         .send(body)
         .expect(201, done);
+    });
+
+    it('should send favorite event to User Engagement Analysis', () => {
+      sinon.spy(helpers, 'sendEvent');
+
+      const body = {
+        tweet_id: 'testTweet',
+        favoriter_id: 'testUser',
+      };
+
+      request
+        .post('/favorite/destroy')
+        .send(body)
+        .then(res => {
+          expect(helpers.sendEvent.to.have.been.calledWith('engagement', '/favorite/destroy', body));
+          done();
+        });
     });
   });
 
@@ -45,6 +82,23 @@ describe('Client Service Events', () => {
         .send(body)
         .expect(201, done);
     });
+
+    it('should send follow event to User Engagement Analysis', () => {
+      sinon.spy(helpers, 'sendEvent');
+
+      const body = {
+        follower_id: 'testUser1', // authenticated user doing the following
+        followed_id: 'testUser2' // id of the person to be followed
+      };
+
+      request
+        .post('/follow/create')
+        .send(body)
+        .then(res => {
+          expect(helpers.sendEvent.to.have.been.calledWith('engagement', '/follow/create', body));
+          done();
+        });
+    });
   });
 
   describe('POST requests to /follow/destroy', () => {
@@ -59,19 +113,55 @@ describe('Client Service Events', () => {
         .send(body)
         .expect(201, done);
     });
+
+    it('should send follow event to User Engagement Analysis', () => {
+      sinon.spy(helpers, 'sendEvent');
+
+      const body = {
+        follower_id: 'testUser1', // authenticated user doing the following
+        followed_id: 'testUser2' // id of the person to be followed
+      };
+
+      request
+        .post('/follow/destroy')
+        .send(body)
+        .then(res => {
+          expect(helpers.sendEvent.to.have.been.calledWith('engagement', '/follow/destroy', body));
+          done();
+        });
+    });
   });
 
   describe('POST requests to /tweets', () => {
     it('should respond with a 201 status code', (done) => {
       const body = {
         user_id: 'testUser1',
-        status: 'Maybe he\'ll finally find his keys. #peterfalk'
+        status: 'Maybe he\'ll finally find his keys. #peterfalk',
+        source: 'Safar for iOS'
       };
 
       request
         .post('/tweets')
         .send(body)
         .expect(201, done);
+    });
+
+    it('should send tweet event to User Engagement Analysis', () => {
+      sinon.spy(helpers, 'sendEvent');
+
+      const body = {
+        user_id: 'testUser1',
+        status: 'Maybe he\'ll finally find his keys. #peterfalk',
+        source: 'Safari for iOS'
+      };
+
+      request
+        .post('/tweets')
+        .send(body)
+        .then(res => {
+          expect(helpers.sendEvent.to.have.been.calledWith('engagement', '/favorite/create', body));
+          done();
+        });
     });
   });
 });
