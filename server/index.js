0721197da082f06a112a0e566e3a9b8c6350edc8
aploftus <aploftus@users.noreply.github.com>
2017-12-18 17:25:11 -0800

    (feat) Set up event for POST requests to correct service

--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const port = 3000;
-module.exports.port = port;
+
+const helpers = require('./helpers.js');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+const engagementPaths = [
+  '/favorite/create',
+  '/favorite/destroy',
+  '/follow/create',
+  '/follow/destroy'
+];
+
+app.post(engagementPaths, (req, res) => {
+  helpers.sendEvent('engagement', req.path, req.body);
+  res.status(201).send();
+});
+
+app.post('/tweets', (req, res) => {
+  helpers.sendEvent('tweets', '/tweets', req.body);
+  res.status(201).send();
+});
+
 app.listen(port, () => console.log(`App listening at http://localhost:${port}!`));
+
+module.exports = app;
