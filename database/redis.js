c0e94359fd57669ba6d42115f16232438fe31363
aploftus <aploftus@users.noreply.github.com>
2017-12-19 15:15:06 -0800

    (feat) Implement server utils serveLocalFeed and userAccessed

--- database/redis.js
+++ database/redis.js
@@ -1,9 +1,8 @@
 const redis = require('redis');
 const client = redis.createClient();
 
-client.select(2, () => {
-  client.on('error', (err) => {
-    console.log('Error ' + err);
-  });
-  client.quit();
+client.on('error', (err) => {
+  console.log('Error ' + err);
 });
+
+module.exports = client;
