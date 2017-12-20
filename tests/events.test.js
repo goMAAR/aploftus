a0be5e3ed33631552decff20d2648b88a504c611
aploftus <aploftus@users.noreply.github.com>
2017-12-20 14:40:14 -0800

    (test) Update tests for favorite and follow update helpers

--- tests/events.test.js
+++ tests/events.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai');
+const { expect, should } = require('chai');
 const moment = require('moment');
 const sinon = require('sinon');
 const supertest = require('supertest');
@@ -8,6 +8,27 @@ const server = require('../server/index.js');
 const request = supertest.agent(server);
 
 describe('Client Service Events', () => {
+  describe('updateFavorite', () => {
+    it('should resolve to an updated favorite object', (done) => {
+      const body = {
+        tweet_id: 1,
+        favoriter_id: 1,
+        author_id: 2,
+        destroy: true
+      };
+
+      utils.updateFavorite(body)
+        .then(favoriteObj => {
+          expect(favoriteObj).to.be.an('object');
+          expect(favoriteObj.favoriter_id).to.be.a('number');
+          expect(favoriteObj.user_favorites_count).to.be.a('number');
+          expect(favoriteObj.tweet_id).to.be.a('number');
+          expect(favoriteObj.favorite_count).to.be.a('number');
+          done();
+        });
+    });
+  });
+
   describe('POST requests to /favorite', () => {
     it('should respond with a 201 status code', (done) => {
       const body = {
@@ -23,26 +44,23 @@ describe('Client Service Events', () => {
         .expect(201, done);
     });
 
-    it('should respond with favorite count object', (done) => {
-      const body = {
-        tweet_id: 1,
-        favoriter_id: 1,
-        author_id: 2,
-        destroy: true
-      };
+    // it('should update favorite counts', (done) => {
+    //   let spy = sinon.spy(utils, 'updateFavorite');
+    //   const body = {
+    //     tweet_id: 1,
+    //     favoriter_id: 1,
+    //     destroy: false
+    //   };
 
-      request
-        .post('/favorite')
-        .send(body)
-        .then(res => {
-          expect(res.body).to.be.an('object');
-          expect(res.body.user_id).to.be.a('number');
-          expect(res.body.user_favorites_count).to.be.a('number');
-          expect(res.body.tweet_id).to.be.a('number');
-          expect(res.body.favorite_count).to.be.a('number');
-          done();
-        });
-    });
+    //   request
+    //     .post('/favorite')
+    //     .send(body)
+    //     .then(res => {
+    //       console.log(res);
+    //       spy.should.have.been.calledWith(body);
+    //       done();
+    //     });
+    // });
 
     it('should send favorite event to User Engagement Analysis', () => {
       sinon.spy(utils, 'sendEvent');
@@ -64,6 +82,26 @@ describe('Client Service Events', () => {
     });
   });
 
+  describe('updateFollow', () => {
+    it('should resolve to an updated follow object', (done) => {
+      const body = {
+        follower_id: 1,
+        followed_id: 2,
+        destroy: false
+      };
+
+      utils.updateFollow(body)
+        .then(followObj => {
+          expect(followObj).to.be.an('object');
+          expect(followObj.follower_id).to.be.a('number');
+          expect(followObj.followers_count).to.be.a('number');
+          expect(followObj.followed_id).to.be.a('number');
+          expect(followObj.friends_count).to.be.a('number');
+          done();
+        });
+    });
+  });
+
   describe('POST requests to /follow', () => {
     it('should respond with a 201 status code', (done) => {
       const body = {
@@ -78,25 +116,24 @@ describe('Client Service Events', () => {
         .expect(201, done);
     });
 
-    it('should respond with follow count object', (done) => {
-      const body = {
-        follower_id: 1,
-        followed_id: 2,
-        destroy: true
-      };
+    // it('should update follow counts', (done) => {
+    //   let spy = sinon.spy(utils, 'updateFollow');
+    //   const body = {
+    //     follower_id: 1,
+    //     followed_id: 2,
+    //     destroy: false
+    //   };
 
-      request
-        .post('/follow')
-        .send(body)
-        .then(res => {
-          expect(res.body).to.be.an('object');
-          expect(res.body.user_id).to.be.a('number');
-          expect(res.body.followers_count).to.be.a('number');
-          done();
-        });
-    });
+    //   request
+    //     .post('/favorite')
+    //     .send(body)
+    //     .then(res => {
+    //       expect(spy).to.have.been.calledWith(body);
+    //       done();
+    //     });
+    // });
 
-    it('should send favorite event to User Engagement Analysis', () => {
+    it('should send follow event to User Engagement Analysis', () => {
       const body = {
         follower_id: 1,
         followed_id: 2,
@@ -116,7 +153,7 @@ describe('Client Service Events', () => {
   describe('POST requests to /tweets', () => {
     it('should respond with a 201 status code', (done) => {
       const body = {
-        user_id: -1,
+        user_id: 1,
         status: 'Maybe he\'ll finally find his keys. #peterfalk',
         source: 'Safari for iOS'
       };
@@ -129,7 +166,7 @@ describe('Client Service Events', () => {
 
     it('should respond with a full tweet object', (done) => {
       const body = {
-        user_id: -1,
+        user_id: 1,
         status: 'Maybe he\'ll finally find his keys. #peterfalk',
         source: 'Safari for iOS'
       };
