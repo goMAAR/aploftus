27266133d658f17a527fb09a28c2bd688bac05d3
aploftus <aploftus@users.noreply.github.com>
2017-12-27 16:41:18 -0800

    (feat) Load test with artillery

--- data-generation/helpers.js
+++ data-generation/helpers.js
@@ -4,15 +4,60 @@ const randomInt = (max, min = 0) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-const fakeHex = () => {
-  let result = '';
-  for (let x = 0; x < 6; x++) {
-    result += hex[randomInt(16)];
-  }
-  return result;
-};
+const sources = ['Safari for iOS', 'Twitter for Mac', 'Twitter for Android', 'Twitter for iOS'];
+
+const prefaces = [
+  'Ask me why I think',
+  'After much consideration I have finally decided that',
+  'Today made me realize',
+  'Have you ever thought about how',
+];
+const nouns = [
+  'pumpkin spice lattes',
+  'dogs that skateboard',
+  'critically acclaimed foreign films',
+  'all of my ideas',
+  'old timey hats',
+  'footlong sandwiches from Subway',
+  'chatty cashiers',
+  'my imaginary friends from elementary school'
+];
+const adjs = [
+  'awesome',
+  'stupid',
+  'amazing',
+  'awful',
+  'whatever',
+  'fine',
+  'a thing',
+  'meh'
+];
 
 module.exports = {
   randomInt: randomInt,
-  fakeHex: fakeHex
+
+  fakeHex: () => {
+    let result = '';
+    for (let x = 0; x < 6; x++) {
+      result += hex[randomInt(16)];
+    }
+    return result;
+  },
+
+  fakeBool: () => {
+    let bools = [true, false];
+    return bools[Math.floor(Math.random() * 2)];
+  },
+
+  fakeSource: () => {
+    return `${sources[randomInt(4)]}`;
+  },
+
+  fakeTweetText: () => {
+    let preface = prefaces[randomInt(4)];
+    let noun = nouns[randomInt(8)];
+    let adj = adjs[randomInt(8)];
+
+    return `${preface} ${noun} are ${adj}`;
+  }
 };
--- data-generation/tweetGeneratorHelper.js
+++ data-generation/tweetGeneratorHelper.js
@@ -1,61 +1,25 @@
 const faker = require('faker');
 const moment = require('moment');
-const { randomInt } = require('./helpers');
-
-const sources = ['Safari for iOS', 'Twitter for Mac', 'Twitter for Android', 'Twitter for iOS'];
-const prefaces = [
-  'Ask me why I think',
-  'After much consideration I have finally decided that',
-  'Today made me realize',
-  'Have you ever thought about how',
-];
-const nouns = [
-  'pumpkin spice lattes',
-  'dogs that skateboard',
-  'critically acclaimed foreign films',
-  'all of my ideas',
-  'old timey hats',
-  'footlong sandwiches from Subway',
-  'chatty cashiers',
-  'my imaginary friends from elementary school'
-];
-const adjs = [
-  'awesome',
-  'stupid',
-  'amazing',
-  'awful',
-  'whatever',
-  'fine',
-  'a thing',
-  'meh'
-];
-
-const fakeTweetText = () => {
-  let preface = prefaces[randomInt(4)];
-  let noun = nouns[randomInt(8)];
-  let adj = adjs[randomInt(8)];
-
-  return `${preface} ${noun} are ${adj}`;
-};
+const helpers = require('./helpers');
 
 const fakeTweet = () => {
   let tweet = {
     // id: assigned by generator function, a num from 0 to numUsers
-    text: fakeTweetText(),
+    text: helpers.fakeTweetText(),
     truncated: false,
     // created_at: moment(faker.date.recent()).format('ddd MMM D hh:mm:ss ZZ YYYY'),
     created_at: moment(faker.date.recent()).format('YYYY-MM-DD hh:mm:ss.SSS'),
-    reply_count: randomInt(500),
-    favorite_count: randomInt(700),
+    reply_count: helpers.randomInt(500),
+    favorite_count: helpers.randomInt(700),
     // favorited: false,
-    retweet_count: randomInt(800),
+    retweet_count: helpers.randomInt(800),
     // retweeted: true,
     in_reply_to_user_id: -1,
     in_reply_to_screen_name: -1,
     in_reply_to_status_id: -1,
     possibly_sensitive: false,
-    source: `${sources[randomInt(4)]}`,
-    user_id: randomInt(2000000),
+    source: helpers.fakeSource(),
+    user_id: helpers.randomInt(2000000),
     // 'user:following': false
   };
   return tweet;
